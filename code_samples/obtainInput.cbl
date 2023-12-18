1a2a1eaf822e1dcd43bc7586b048f9d62d26f262
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-12-18 17:20:38 +0000

    feat: receive user input to print costumizable message

--- code_samples/obtainInput.cbl
+++ code_samples/obtainInput.cbl
@@ -10,11 +10,15 @@
       *(Level number: A level number is a one-digit or two-digit int
       *between 01 and 49, or one of three special level numbers: 66, 77, or 88)  
       *01 -> the lowest level number === highest level of element.
+
+      *PIC (picture) clause -> byte-by-byte def of the format of a var.
        01 NAME PIC A(20).
     
      
        PROCEDURE DIVISION. 
            0100-START-HERE.
-               DISPLAY "Hello World!".
+               DISPLAY "Hello! Please enter your name:".
+               ACCEPT NAME.
+               DISPLAY "It's nice to meet you, ", NAME.
        STOP RUN.
        END PROGRAM OBTAININPUT.
